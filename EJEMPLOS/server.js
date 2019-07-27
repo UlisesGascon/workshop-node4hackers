6fa642c50790905d9b5c6482c80f8aca137cd0cb
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-07-27 10:49:19 +0200

    basic server

---
+++ EJEMPLOS/server.js
@@ -0,0 +1,14 @@
+//import http from 'http';
+const http = require('http');
+
+const port = 3000;
+const ip = "127.0.0.1";
+const msg = JSON.stringify({msg: 'Hola a todos, ahora usando HTTP'});
+
+
+http.createServer((req, res) => {
+    console.log(req.headers)
+  res.writeHead(666, {'Content-Type': 'application/json'});
+  res.end(msg);
+}).listen(port, ip);
+console.log(`Server running at http://${ip}:${port}/`);
