acc5cd67f1a478f0bf710f90e9752f019ad47ea4
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-07-27 11:41:37 +0200

    express basico

---
+++ proxy-cosas/basic-server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.get('/name', function (req, res) {
+
+    res.send(`Hola ${req.query.nombre}!`)
+})
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
+})
