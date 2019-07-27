7ec4f18c7a8488979112e1e0f3639ebf90686f53
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-07-27 10:40:29 +0200

    mas cambios

--- EJEMPLOS/asincronia_basica.js
+++ EJEMPLOS/asincronia_basica.js
@@ -1,12 +1,35 @@
+/*  ----- callbacks ----*/ 
 function sleep (callback) {
     setTimeout(() => {
         console.log("Hola mundo");
         callback()
     }, 1000);
 }
-
+/*
 console.log("---- inicio ----")
 
 sleep(function () {
     console.log("---- fin -----")
 })
+*/
+
+
+
+/* ---- PROMSESAS ---- */
+function sleepPromise (callback) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Hola mundo");
+            resolve()
+        }, 1000);
+    })
+
+}
+
+console.log("---- inicio ----")
+
+sleepPromise()
+    .then(sleepPromise)
+    .then(function () {
+        console.log("---- fin -----")
+    })
